2fb506fee7506e3484802b1407dadd0cc10abbe8
tracht <marija.trachtenberg@gmail.com>
2020-04-09 10:48:23 +0100

    Interactive menu added

--- directory.rb
+++ directory.rb
@@ -1,24 +1,25 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just type \"done\" "
+  puts "Please enter the student's"
+  puts "name, cohort, degree, and course"
+  puts "To finish, type \"done\" "
   # create an empty array
-  @students = []
-
+  students = []
   name = gets.chomp
   while name != "done" do
     cohort = gets.chomp
     degree = gets.chomp
     course = gets.chomp
     # add the student hash to the array
-    @students << {name: name, cohort: cohort, degree: degree, course: course}
-      if @students.count == 1
-        puts "Now we have #{@students.count} student"
-      elsif @students.count != 1
-        puts "Now we have #{@students.count} students"
+    students << {name: name, cohort: cohort, degree: degree, course: course}
+      if students.count == 1
+        puts "Now we have #{students.count} student"
+      elsif students.count != 1
+        puts "Now we have #{students.count} students"
       end
       # get the next name
       name = gets.chomp
   end
+  students
 end
 
 def print_header
@@ -26,13 +27,17 @@ def print_header
   puts "------------"
 end
 
-def print_footer(students_list)
-  puts "Overall, we have #{students_list.count} great students"
+def print_footer(students)
+  if students.count == 1
+    puts "Overall, we have #{students.count} great student"
+  else
+    puts "Overall, we have #{students.count} great students"
+  end
 end
 
-def print_list(students_list)
-  students_list.each_with_index do |student, index|
-    puts " #{index+1}. #{student[:name]}, #{student[:cohort]} cohort, #{student[:degree]} of #{student[:course]} "
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index+1}. #{student[:name]}, #{student[:cohort]} cohort, #{student[:degree]} of #{student[:course]}"
   end
 end
 
@@ -82,19 +87,47 @@ def filter_by_cohort_each(students_list, criteria)
 end
 
 def filter_by_cohort_map(students_list, criteria)
-  puts "Students in the cohort of #{criteria}"
+  new_array = []
   students_list.map do |student|
     if student[:cohort] == criteria.to_s
-      puts student[:name] + " " + student[:degree] +  " " + student[:course]
+      new_array.push(student[:name] + ", " + student[:degree] +  ", " + student[:course])
+    end
+  end
+  puts "Students in the cohort of #{criteria}"
+  puts "total of: #{new_array.count}"
+  puts new_array
+end
+
+def typo_correction()
+
+end
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. Print the menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    # 2. Read the input and save it into a variable
+    selection = gets.chomp
+    # 3. Do what the user asked
+    case selection
+      when "1"
+        # input students
+        students = input_students
+      when "2"
+        # show students
+        print_header()
+        print1(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't understand, try again"
     end
   end
 end
 
 #nothing happens until we call the methods
-input_students()
-# print_header()
-# print_list (@students)
-# filter_students_start_with(@students, "C")
-# filter_name_length(@students, 4)
-filter_by_cohort_map(@students, 2020)
-filter_by_cohort_each(@students, 2020)
+interactive_menu()
