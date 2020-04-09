b269ae932685a87f9ccff180e64740aa6f244603
tracht <marija.trachtenberg@gmail.com>
2020-04-09 14:38:32 +0100

    Created methods to deal with typos, improved interactive menu

--- directory.rb
+++ directory.rb
@@ -1,14 +1,25 @@
 def input_students
-  puts "Please enter the student's"
-  puts "name, cohort, degree, and course"
+  puts "Please enter the student's name, cohort, degree, & course"
   puts "To finish, type \"done\" "
   # create an empty array
   students = []
+  puts "Enter student's name"
   name = gets.chomp
+  name = edit_name(name)
+
   while name != "done" do
+    puts "Enter student's cohort"
     cohort = gets.chomp
+    cohort = edit_cohort(cohort)
+
+    puts "Enter student's degree: MSc, BA, BSc"
     degree = gets.chomp
+    degree = edit_degree(degree)
+
+    puts "Enter student's course, i.e. Maths"
     course = gets.chomp
+    course = edit_course(course)
+
     # add the student hash to the array
     students << {name: name, cohort: cohort, degree: degree, course: course}
       if students.count == 1
@@ -22,16 +33,90 @@ def input_students
   students
 end
 
+def edit_name(name)
+  puts ">> Would you like to edit their name? Type yes or no"
+  edit_name = gets.chomp.downcase
+    loop do
+      if edit_name == "yes"
+        puts ">> Edit name"
+        name = gets.chomp
+        puts ">> Name has been updated"
+        break
+      elsif edit_name == "no"
+        break
+      else
+        puts ">> I don't understand, try again"
+      end
+    end
+  return name
+end
+
+def edit_cohort(cohort)
+  puts ">> Would you like to edit the cohort? Type yes or no"
+  edit_cohort = gets.chomp.downcase
+    loop do
+      if edit_cohort == "yes"
+        puts ">> Edit cohort"
+        cohort = gets.chomp
+        puts ">> Cohort has been updated"
+        break
+      elsif edit_cohort == "no"
+        break
+      else
+        puts ">> I don't understand, try again"
+      end
+    end
+  return cohort
+end
+
+def edit_degree(degree)
+  puts ">> Would you like to edit the degree? Type yes or no"
+  edit_degree = gets.chomp.downcase
+    loop do
+      if edit_degree == "yes"
+        puts ">> Edit degree"
+        degree = gets.chomp
+        puts ">> Degree has been updated"
+        break
+      elsif edit_degree == "no"
+        break
+      else
+        puts ">> I don't understand, try again"
+      end
+    end
+  return degree
+end
+
+def edit_course(course)
+  puts ">> Would you like to edit the course? Type yes or no"
+  edit_course = gets.chomp.downcase
+    loop do
+      if edit_course == "yes"
+        puts ">> Edit course"
+        course = gets.chomp
+        puts ">> Course has been updated"
+        break
+      elsif edit_course == "no"
+        break
+      else
+        puts ">> I don't understand, try again"
+      end
+    end
+  return course
+end
+
 def print_header
-  puts "The students of Villains Academy"
-  puts "------------"
+  puts "The students of Villains Academy:"
 end
 
 def print_footer(students)
+  puts ">>"
   if students.count == 1
-    puts "Overall, we have #{students.count} great student"
+    puts ">> Overall, we have #{students.count} great student"
+    puts ""
   else
-    puts "Overall, we have #{students.count} great students"
+    puts ">> Overall, we have #{students.count} great students"
+    puts ""
   end
 end
 
@@ -106,9 +191,11 @@ def interactive_menu
   students = []
   loop do
     # 1. Print the menu
+    puts "----MENU----"
     puts "1. Input the students"
     puts "2. Show the students"
     puts "9. Exit"
+    puts "-----------"
     # 2. Read the input and save it into a variable
     selection = gets.chomp
     # 3. Do what the user asked
@@ -119,7 +206,7 @@ def interactive_menu
       when "2"
         # show students
         print_header()
-        print1(students)
+        print(students)
         print_footer(students)
       when "9"
         exit
